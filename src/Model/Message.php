5c229b567497376a59332c21a24fbd482c1ba622
Riccardo Albero <albero180@gmail.com>
2023-09-14 15:44:45 +0900

    add prompt settings support

--- src/Model/Message.php
+++ src/Model/Message.php
@@ -7,14 +7,17 @@ class Message
     public string $text;
     public string $user_id = '';
 
+    public array $prompt_settings = [];
+
     /**
      * @param string $text
      * @param string $user_id
      */
-    public function __construct(string $text, string $user_id = '')
+    public function __construct(string $text, string $user_id = '', array $prompt_settings = [])
     {
         $this->text = $text;
         $this->user_id = $user_id;
+        $this->prompt_settings = $prompt_settings;
     }
 
 
